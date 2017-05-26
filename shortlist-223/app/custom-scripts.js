f8e0a4cdd629e7e7240c657545eb074b7ebc3a53
Patrick McKinney <pnmcartography@gmail.com>
2017-05-26 14:56:04 -0400

    add base script customizations and call stylesheets for ver 2.2.3

--- shortlist-223/app/custom-scripts.js
+++ shortlist-223/app/custom-scripts.js
@@ -8,5 +8,37 @@ define(["dojo/topic"], function(topic) {
 		/*
 		* Custom Javascript to be executed when the application is ready goes here
 		*/
+        // Last Data Update Text
+        var lastDataUpdateDate = '4-19-2017'
+        var dataUpdateText = '<span id="dataUpdate">Data Last Updated: ' + lastDataUpdateDate + '</span>';
+        var ccpaMobileTitlePage = $('#mobileTitlePage');
+        var ccpaHeader = $('#header');
+    
+        $(document).ready(function() {
+            var viewportWidth =$(window).width();
+            if (viewportWidth >= 768 ) {
+                ccpaHeader.append(dataUpdateText);
+            } else {
+                ccpaMobileTitlePage.append(dataUpdateText);    
+            }
+        });
+
+        $(window).resize(function() {
+            var viewportWidth = $(window).width();
+            if (viewportWidth >= 768) {
+                ccpaHeader.append(dataUpdateText);
+            }
+        });
+        
+        // Custom Legend
+        var viewportWidth = window.innerWidth;
+        // Custom Legend - If screen width is above breakpoint, display legend button
+        if (viewportWidth > 767) {
+            $('#customLegendBtn').show();
+        } 
+        // Custom Legend - ddd display/hide functionality to Legend button
+        $('#customLegendBtn').click(function () {
+             $('#customLegend').toggle();
+        });
 	});
 });
