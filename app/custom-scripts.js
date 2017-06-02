eb8ffee44879ce1e1687aabe23a2786d500db5dc
Patrick McKinney <pnmcartography@gmail.com>
2017-06-02 16:22:20 -0400

    update custom script and call stylesheets in index.html

--- app/custom-scripts.js
+++ app/custom-scripts.js
@@ -8,5 +8,43 @@ define(["dojo/topic"], function(topic) {
 		/*
 		* Custom Javascript to be executed when the application is ready goes here
 		*/
+        // Last Data Update Text
+        var lastDataUpdateDate = '4-19-2017' // update text to last data update date
+        var dataUpdateText = '<span id="dataUpdate">Data Last Updated: ' + lastDataUpdateDate + '</span>';
+        var ccpaMobileTitlePage = $('#mobileTitlePage');
+        var ccpaHeader = $('#header');
+        var viewportWidth = $(window).width();
+        // Custom Legend
+        var customLegendBtn = $('#customLegendBtn');        
+    
+        $(document).ready(function() {
+            if (viewportWidth >= 768 ) {
+                ccpaHeader.append(dataUpdateText);
+            } else {
+                ccpaMobileTitlePage.append(dataUpdateText);    
+            }
+        });
+
+        $(window).resize(function() {
+            if (viewportWidth >= 768) {
+                ccpaHeader.append(dataUpdateText);
+            }
+        });
+        
+        // Custom Legend
+        var viewportWidth = window.innerWidth;
+        // Custom Legend - If screen width is above breakpoint, display legend button
+        if (viewportWidth > 767) {
+            customLegendBtn.show();
+        } else {
+            $('#mobileThemeList .mobileTitleThemes').click(function() {
+                customLegendBtn.show();
+            });
+        }        
+        
+        // Custom Legend - add display/hide functionality to Legend button
+        customLegendBtn.click(function () {
+             $('#customLegend').toggle();
+        });
 	});
 });
