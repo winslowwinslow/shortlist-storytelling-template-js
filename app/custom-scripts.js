9976dd4edcc86da5ed7636aef3c6e8dce39bcd54
Patrick McKinney <pnmcartography@gmail.com>
2017-10-13 09:12:43 -0400

    update docs for custom js file. remove old transition files

--- app/custom-scripts.js
+++ app/custom-scripts.js
@@ -10,41 +10,46 @@ define(["dojo/topic"], function(topic) {
 		*/
         // Last Data Update Text
         var lastDataUpdateDate = '4-19-2017' // update text to last data update date
+        // Element that will contain the last data update text
         var dataUpdateText = '<span id="dataUpdate">Data Last Updated: ' + lastDataUpdateDate + '</span>';
+        // Mobile title page element
         var ccpaMobileTitlePage = $('#mobileTitlePage');
+        // Header Element
         var ccpaHeader = $('#header');
+        // Viewport Width
         var viewportWidth = $(window).width();
-        // Custom Legend
+        // Custom Legend Button
         var customLegendBtn = $('#customLegendBtn');        
     
+        // Append last data update text to appropriate element based upon viewport size
         $(document).ready(function() {
+            // above breakpoint
             if (viewportWidth >= 768 ) {
                 ccpaHeader.append(dataUpdateText);
             } else {
-                ccpaMobileTitlePage.append(dataUpdateText);    
+                ccpaMobileTitlePage.append(dataUpdateText); // below breakpoint    
             }
         });
 
+        // place last data update text on header if screen size changes to above breakpoint    
         $(window).resize(function() {
             if (viewportWidth >= 768) {
                 ccpaHeader.append(dataUpdateText);
             }
-        });
+        });        
         
-        // Custom Legend
-        var viewportWidth = window.innerWidth;
         // Custom Legend - If screen width is above breakpoint, display legend button
         if (viewportWidth > 767) {
             customLegendBtn.show();
         } else {
             $('#mobileThemeList .mobileTitleThemes').click(function() {
-                customLegendBtn.show();
+                customLegendBtn.show(); // show legend button once map is visible below breakpoint
             });
         }        
         
         // Custom Legend - add display/hide functionality to Legend button
         customLegendBtn.click(function () {
-             $('#customLegend').toggle();
+             $('#customLegend').toggle(); // hide/show legend when legend button clicked
         });
 	});
 });
